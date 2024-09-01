2615ae16e21c0fbf6494f06dfe363b24e8d46355
Cadons <matteo.cadoni01@gmail.com>
2024-09-01 19:40:21 +0200

    Update test directory name in exports_sources

--- conanfile.py
+++ conanfile.py
@@ -21,7 +21,7 @@ class libtusclientRecipe(ConanFile):
     default_options = {"shared": False, "fPIC": True}
 
     # Sources are located in the same place as this recipe, copy them to the recipe
-    exports_sources = "CMakeLists.txt", "src/*", "include/*", "tests/*", "config.h"
+    exports_sources = "CMakeLists.txt", "src/*", "include/*", "test/*", "config.h"
 
     def get_version(self):
         git = Git(self)
